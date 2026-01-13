ba76333309f19d461943d2ffc2907aff80012569
Sai <tomato.202504.tsoimk@gmail.com>
2026-01-13 13:28:14 +0900

    route adjust

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { GamePageComponent } from './page/game-page/game-page.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'game', pathMatch: 'full' },
     { path: 'game', component: GamePageComponent },
 ];
--- frontend/src/app/services/socket.service.ts
+++ frontend/src/app/services/socket.service.ts
@@ -7,6 +7,8 @@ export class SocketService {
   private socket!: Socket;
 
   connect() {
+    if (this.socket) return;
+
     this.socket = io('http://localhost:3000', {
       transports: ['websocket'],
     });
@@ -29,6 +31,7 @@ export class SocketService {
   }
 
   disconnect() {
-    this.socket.disconnect();
+    this.socket?.disconnect();
+    this.socket = undefined as any;
   }
 }
