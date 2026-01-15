216c0c346a09f47440cf08d89a1361c2441f2fff
ブライアン <tomato.202504.brianm@gmail.com>
2026-01-15 16:00:50 +0900

    update wall.ts

--- frontend/src/game/entities/wall.ts
+++ frontend/src/game/entities/wall.ts
@@ -2,35 +2,47 @@ import Phaser from 'phaser';
 
 export class Wall{
 readonly body: number;
-sprite: Phaser.GameObjects.Rectangle;
+topWall: Phaser.GameObjects.Rectangle;
+bottomWall: Phaser.GameObjects.Rectangle;
+private wallgap:number=150;
 
     constructor (
         private scene: Phaser.Scene, 
         config: {id:number; x: number; y: number; })
     {
         this.body=config.id;
-        this.sprite = scene.add.rectangle(
+        const wallHeight=600;
+        const wallWidth=32;
+
+        this.topWall = scene.add.rectangle(
             config.x,
             config.y,
-            32,
-            100,
+            wallWidth,
+            wallHeight,
             0xffff00
         );
+        this.topWall.setOrigin(0.5,1); // Set origin to bottom center
+
+        this.bottomWall = scene.add.rectangle(
+            config.x,
+            config.y + this.wallgap, // Adjusted for gap
+            wallWidth,
+            wallHeight,
+            0xffff00
+        );
+        this.bottomWall.setOrigin(0.5,0); // Set origin to top center
     }
 
     update():boolean{
 
-        const speed = 8;
-        this.sprite.x -= speed;
-        if(this.sprite.x<=0){
-            this.sprite.destroy();
+        const speed = 5 ;
+        this.topWall.x -= speed;
+        this.bottomWall.x -= speed;
+        if(this.topWall.x<=0){
+            this.topWall.destroy();
+            this.bottomWall.destroy();
             return false;
         }
         else return true;
-    }
-
-    spawn (x:number, y:number)
-    {
-        this.sprite.setPosition(x, y);
     }
 }
